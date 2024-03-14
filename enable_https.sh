a51cba6ea6eb52ed49caa86f614f905eba0a1b7d
Tony Bourke <tony.bourke@sdn-pros.com>
2024-03-13 17:57:14 -0700

    Added config.yaml changing

--- enable_https.sh
+++ enable_https.sh
@@ -1,12 +1,21 @@
 # This script changes the code-server config file to allow access via 443
 
+if [ -e ~/.config/code-server/config.yaml]
+then 
+    echo "Code-server installation detected, proceeding"
+else
+    echo "config.yaml file not found, please install code-server"
+    exit
+fi
+
 echo "What password would you like to set for access to code-server?"
 read -s password
 
-echo "auth: password" > testconfig
-echo "bind-addr: 0.0.0.0:8080" >> testconfig
-echo "cert: true" >> testconfig
-echo "password: $password" >> testconfig
+cp ~/.config/code-server/config.yaml ~/.config/code-server/config.yaml.orig
+echo "auth: password" > ~/.config/code-server/config.yaml
+echo "bind-addr: 0.0.0.0:8080" >> ~/.config/code-server/config.yaml
+echo "cert: true" >> ~/.config/code-server/config.yaml
+echo "password: $password" >> ~/.config/code-server/config.yaml
 
 # Allow code-server to answer on 443 even though it's running as a user
 #sudo setcap cap_net_bind_service=+ep /usr/lib/code-server/lib/node
