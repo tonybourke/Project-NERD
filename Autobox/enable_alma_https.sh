cb6b5c65a3d1a6cd36d396355370fb88abf118af
Tony Bourke <Tony Bourke>
2025-03-10 01:20:12 -0700

    Fixed hashing parts

--- Autobox/enable_alma_https.sh
+++ Autobox/enable_alma_https.sh
@@ -1,7 +1,7 @@
 # This script changes the code-server config file to allow access via 8080
 
 if [ -e ~/.config/code-server/config.yaml ]
-then 
+then
     echo "Code-server installation detected, proceeding"
 else
     echo "config.yaml file not found, please install and enable code-server"
@@ -14,6 +14,7 @@ fi
 curl -fsSL https://raw.githubusercontent.com/tonybourke/Project-NERD/refs/heads/main/Autobox/mkargon2hash.py > mkargon2hash.py
 
 
+echo "Installing Argon2 Python3 module (for making a secure hash)"
 echo "What password would you like to set for access to code-server?"
 read -s password
 
@@ -21,14 +22,12 @@ cp ~/.config/code-server/config.yaml ~/.config/code-server/config.yaml.orig
 echo "auth: password" > ~/.config/code-server/config.yaml
 echo "bind-addr: 0.0.0.0:8080" >> ~/.config/code-server/config.yaml
 echo "cert: true" >> ~/.config/code-server/config.yaml
-hashpassword = python3 mkargon2hash.py $password 
-echo "hashed_passwrd: $password" >> ~/.config/code-server/config.yaml
+hashpassword=$(python3 mkargon2hash.py $password)
+echo $hashpassword
+echo "hashed-password: $hashpassword" >> ~/.config/code-server/config.yaml
 echo "Now restarting code-server to activate new settings. The config file can be edited at ~/config/code-server/config.yaml"
 sudo systemctl restart code-server@$USER
 
 echo "Configuring the firewall to allow port 8080"
 sudo firewall-cmd --add-port=8080/tcp --permanent
 sudo systemctl restart firewalld
-
-
-
